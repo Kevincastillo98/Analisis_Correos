fed485ca2959faa275d482221d09e672e742b6e1
kevin <castillokevin37@gmail.com>
2019-08-22 04:20:00 -0500

    Se ha mdificado el archivo correos.py

--- correos.py
+++ correos.py
@@ -1,9 +1,10 @@
 import csv
 import  re
 import matplotlib.pyplot as plt
+import seaborn as sns
 import collections
+import statistics
 from collections import Counter
-
 
 
 texto = open("emails.csv","r").read()
@@ -12,18 +13,13 @@ texto = open("emails.csv","r").read()
 ##Se hace uso de regex
 
 emails = re.findall("@.*",texto)
-
 lista = [i.split('.', 1)[0] for i in emails]
-
 salida = Counter(lista)
-
 ##Salida es un diccionario
-
 print(salida)
 
 orden_salida = collections.OrderedDict(sorted(salida.items(), key=lambda t: t[1]))
 print(orden_salida)
-
 
 nueva_lista = salida.keys()
 print(nueva_lista)
@@ -38,8 +34,9 @@ print(genero)
 variable_genero = list(genero.keys())
 cantidad_genero = list(genero.values())
 
+colors = ["#83a598","#d3869b"]
 fig, ax = plt.subplots()
-ax.pie(cantidad_genero, labels=variable_genero, autopct='%1.1f%%')
+ax.pie(cantidad_genero, labels=variable_genero, colors=colors,autopct='%1.1f%%')
 ax.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
 ax.set_title('Variable Sexo')
 plt.show()
@@ -52,16 +49,33 @@ with open('emails.csv') as edad:
 print(edad)
 
 orden_edad = collections.OrderedDict(sorted(edad.items()))
-print(orden_edad)
+print("edades:",orden_edad)
 
 
-plt.bar(list(orden_edad.keys()),orden_edad.values(), color='g')
+#media
+multiplicacion = 0
+div=0
+for k, v in edad.items():
+    multiplicacion += int(k)*int(v)
+    div += v
+media = multiplicacion/div
+print("media:" ,media)
+
+#Mediana
+
+lista_edades = orden_edad.keys()
+resultado = list(map(int,lista_edades))
+mediana =statistics.median(resultado)
+print("mediana:" , mediana)
+
+plt.bar(list(map(int,orden_edad.keys())),orden_edad.values(),edgecolor="black",width=1, color='#8ec07c')
+plt.axvline(media, color='r', linestyle='--')
+plt.axvline(mediana, color='b', linestyle='--')
+plt.grid()
 plt.show()
-
 ##Se crea un bar plot
 
-
-plt.bar(range(len(orden_salida)),list(orden_salida.values()),align='center')
+plt.bar(range(len(orden_salida)),list(orden_salida.values()),edgecolor="black",color="#83a598",align='center')
 plt.xticks(range(len(orden_salida)),list(orden_salida.keys()))
 plt.show()
 
